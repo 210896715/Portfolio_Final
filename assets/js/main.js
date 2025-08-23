3f311dde6794612552f376c6b5c3c6dae9b42fd7
210896715 <zanonibr@hotmail.com>
2025-08-23 12:01:58 -0300

    atualização das skills

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 
+
 function updateProfileInfo(profileData){
 
     const photo = document.getElementById('profile-photo')
@@ -23,11 +24,26 @@ function updateProfileInfo(profileData){
     email.innerText = profileData.email
     email.href = `mailto:${profileData.email}`
 
+
+}
+
+function updateSoftSkills(profileData){
+    const softSkills = document.getElementById('profile.skills.softSkills')
+
+    softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('')
+}
+
+function updateHardSkills(profileData){
+    const hardSkills = document.getElementById('profile.skills.hardSkills')
+
+    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill =>
+         `<li> <img src = "${skill.logo}" alt = "${skill.name}" title = "${skill.name}" > </li>` ).join('')
 }
 
 (async () => {
 
     const profileData = await fetchProfileData()
-    console.log(profileData)
-
+    updateProfileInfo(profileData)
+    updateSoftSkills(profileData)
+    updateHardSkills(profileData)
 })()
