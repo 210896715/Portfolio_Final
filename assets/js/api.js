b488bd3474b1d0834d9e6077ca9f0a6ad63ebfb9
210896715 <zanonibr@hotmail.com>
2025-08-25 10:01:04 -0300

    Atualização dos assets

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,3 @@
-
-
 async function fetchProfileData (){
     const url = 'https://raw.githubusercontent.com/210896715/js-developer-portfolio/refs/heads/patch-1/data/profile.json';
     const fetching = await fetch(url)
